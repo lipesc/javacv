ab5ae9717cd9e0defaaaed5c96807b09455226a0
lipesc <felipedrhouse@hotmail.com>
2025-03-11 15:58:46 -0300

    init: repo base

---
+++ readme.md
@@ -0,0 +1,8 @@
+estudos java  
+
+experimentos com a biblioteca javacv para detectar objetos, pessoas em uma imagem,
+javacv usa wrappers da biblioteca javacpp que são usadas no campo de visão computacional.
+
+
+primeiro ojetivo é detectar um rosto  
+segundo e criar uma interface para fazer o upload e descrever a imagem  
---
+++ src/main/java/org/lipesc/computervision/JavaCV.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.lipesc.computervision;
+
+/**
+ *
+ * @author lipesc
+ */
+public class JavaCV {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
